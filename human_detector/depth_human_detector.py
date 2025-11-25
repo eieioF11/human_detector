7f68360bb99c4cf85b40c4bfcfca5a1575f6088b
f11 <eieioeiji0501@gmail.com>
2025-11-25 18:24:02 +0900

    [update] Update model name and refactor synchronization in HumanDetector

--- human_detector/depth_human_detector.py
+++ human_detector/depth_human_detector.py
@@ -21,7 +21,7 @@ class HumanDetector(Node):
 		super().__init__(self.SELFNODE)
 		self.get_logger().info("%s initializing..." % (self.SELFNODE))
 		# モデルの読み込み
-		model_name = self.param("model_name", 'yolov8x-seg.pt').string_value # yolov8n.pt, yolov8x-seg.pt ,yolov8x-pose.pt
+		model_name = self.param("model_name", 'yolov8m-seg.pt').string_value # yolov8n.pt, yolov8x-seg.pt ,yolov8x-pose.pt
 		self.model = YOLO(model_name)
 		# self.model.export(format="engine")
 		# self.tensorrt_model = YOLO(model_name.replace('.pt', '.engine'))
@@ -30,15 +30,11 @@ class HumanDetector(Node):
 		self.resaul_image_pub_ = self.create_publisher(Image,'human_detector/resaul_image', 1)
 		self.image_sub_ = Subscriber(self,Image,'image_raw', qos_profile=ReliabilityPolicy.RELIABLE)
 		self.depth_mage_sub_ = Subscriber(self,Image,'depth', qos_profile=ReliabilityPolicy.RELIABLE)
-		self.image_sub_ = self.create_subscription(Image,'image_raw', self.image_callback, qos_profile=ReliabilityPolicy.RELIABLE)
-		self.depth_sub_ = self.create_subscription(Image,'depth', self.depth_callback, qos_profile=ReliabilityPolicy.RELIABLE)
-		self.timer_ = self.create_timer(0.001, self.timer_callback)
 		# self.time_sync = TimeSynchronizer([self.image_sub_, self.depth_mage_sub_],100)
-		# self.time_sync = ApproximateTimeSynchronizer([self.image_sub_, self.depth_mage_sub_],100,delay)
-		# self.time_sync.registerCallback(self.sync_callback)
+		delay = 0.05  # Adjust the delay as needed
+		self.time_sync = ApproximateTimeSynchronizer([self.image_sub_, self.depth_mage_sub_],100, delay)
+		self.time_sync.registerCallback(self.sync_callback)
 		self.bridge_ = CvBridge()
-		self.image = None
-		self.depth = None
 		self.get_logger().info("end of %s initializing..." % (self.SELFNODE))
 
 	def __del__(self):
@@ -49,13 +45,7 @@ class HumanDetector(Node):
 		self.declare_parameter(name, value)
 		return self.get_parameter(name).get_parameter_value()
 
-	def image_callback(self, image):self.image = image
-	def depth_callback(self, depth):self.depth = depth
-	def timer_callback(self):
-		if self.depth is not None and self.image is not None:
-			self.sync_callback(self.image, self.depth)
-
-	def sync_callback( self, image, depth):
+	def sync_callback(self, image, depth):
 		print("image_callback")
 		raw_image = self.bridge_.imgmsg_to_cv2(image)
 		d_img = self.bridge_.imgmsg_to_cv2(depth)
